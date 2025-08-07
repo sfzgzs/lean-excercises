1f3cbfb0e0a92d68fc7c74c52469e3e8f36c8a1b
sfzgzs <ghasemi.fateme33@gmail.com>
2025-08-06 22:46:03 -0400

    Fix theorem name -> snake_case

--- 7-Inductive-types/DataType.lean
+++ 7-Inductive-types/DataType.lean
@@ -5,9 +5,9 @@ inductive Hidden.Term
   | Plus : Hidden.Term → Hidden.Term → Hidden.Term
   | Times : Hidden.Term → Hidden.Term → Hidden.Term
 
-def Hidden.termEval (term : Hidden.Term) (vars : Nat → Nat) : Nat :=
+def Hidden.term_eval (term : Hidden.Term) (vars : Nat → Nat) : Nat :=
   match term with
   | .constnt n => n
   | .Var n => vars n
-  | .Plus n m => termEval n vars + termEval m vars
-  | .Times n m => termEval n vars * termEval m vars
+  | .Plus n m => term_eval n vars + term_eval m vars
+  | .Times n m => term_eval n vars * term_eval m vars
