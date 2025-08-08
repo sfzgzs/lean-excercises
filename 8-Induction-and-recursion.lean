82798c0b9ca47d95667a700a7c970565133a5a4e
sfzgzs <ghasemi.fateme33@gmail.com>
2025-08-08 16:26:01 -0400

    Add Expr Example

--- 8-Induction-and-recursion.lean
+++ 8-Induction-and-recursion.lean
@@ -19,3 +19,27 @@ def map (f : α → β → γ) : {n : Nat} → Vect α n → Vect β n → Vect 
 def append_vectors {α : Type u} : {n : Nat} → {m : Nat} → Vect α n → Vect α m → Vect α (m+n)
   | 0, _, .nil, vm => vm
   | _, _, .cons a as, vm => .cons a (append_vectors as vm)
+
+inductive Expr where
+  | const : Nat → Expr
+  | var : Nat → Expr
+  | plus : Expr → Expr → Expr
+  | times : Expr → Expr → Expr
+deriving Repr
+
+def sampleExpr : Expr :=
+  .plus (.times (.var 0) (.const 7)) (.times (.const 2) (.var 1))
+
+def eval (v : Nat → Nat) : Expr → Nat
+  | .const n     => n
+  | .var n       => v n
+  | .plus e₁ e₂  => (eval v e₁) + (eval v e₂)
+  | .times e₁ e₂ =>  (eval v e₁) * (eval v e₂)
+
+def sampleVal : Nat → Nat
+  | 0 => 5
+  | 1 => 6
+  | _ => 0
+
+-- Try it out. You should get 47 here. Done!
+#eval eval sampleVal sampleExpr
