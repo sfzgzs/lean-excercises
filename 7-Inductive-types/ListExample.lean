d5a458bf3a1bb33ecec41113ff6a6ff1f915353a
sfzgzs <ghasemi.fateme33@gmail.com>
2025-08-07 21:01:42 -0400

    Add myReverse, reverse_of_reverse theorem

--- 7-Inductive-types/ListExample.lean
+++ 7-Inductive-types/ListExample.lean
@@ -22,3 +22,24 @@ theorem len_reverse : len (reverse xs) = len xs := by
   induction xs with
   | nil => simp
   | cons head tail hyp => simp [hyp, Nat.add_comm]
+@[simp]
+def myReverse : List Nat → List Nat
+  | nil => nil
+  | cons head tail => myReverse tail ++ [head]
+
+theorem myReverse_append (a b : List Nat) :
+  myReverse (a ++ b) = (myReverse b) ++ (myReverse a) := by
+  induction a with
+  | nil => simp
+  | cons head tail hyp =>
+    simp [List.append_assoc, hyp]
+
+
+theorem myReverse_singleton (x : Nat) :
+  myReverse [x] = [x] := by simp
+
+theorem reverse_of_reverse (l : List Nat) : myReverse (myReverse l) = l := by
+  induction l with
+  | nil => simp
+  | cons head tail hyp =>
+    simp [myReverse_append, hyp]
