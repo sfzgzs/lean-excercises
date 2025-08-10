c1bd5c65d856a399e1c69fc0d8bd3a8ba2d1f423
sfzgzs <ghasemi.fateme33@gmail.com>
2025-08-10 18:25:45 -0400

    Add Polymorphism to Rectangle Example

--- 9-Structures-and-records.lean
+++ 9-Structures-and-records.lean
@@ -1,20 +1,35 @@
-structure Rectangle where
-  width : Nat
-  height : Nat
+class Shape (S : Type u → Type v) (α : Type u) where
+  area : S α → α
 
-def Rectangle.area (r : Rectangle) : Nat :=
-  r.width * r.height
+structure Rectangle (α : Type u) where
+  width : α
+  height : α
+
+structure Triangle (α : Type u) where
+  base : α
+  height : α
+
+instance : Shape Rectangle Nat where
+  area r := r.width * r.height
+
+instance : Shape Triangle Nat where
+  area t := (t.base * t.height) / 2
 
 inductive Color where
   | red | green | blue
+deriving Repr
 
-
-structure ColoredRectangle extends Rectangle where
+structure ColoredRectangle (α : Type u) extends Rectangle α where
   color : Color
 
+instance [Shape Rectangle α] : Shape ColoredRectangle α where
+  area cr := Shape.area cr.toRectangle
 
-def myRectangle : ColoredRectangle :=
+def myRectangle : ColoredRectangle Nat :=
   { width := 5, height := 3, color := Color.blue }
 
+def myTriangle : Triangle Nat :=
+  { base := 10, height := 4 }
 
-example : myRectangle.area = 15 := rfl
+example : Shape.area myRectangle = 15 := rfl
+example : Shape.area myTriangle = 20 := rfl
