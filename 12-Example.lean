389f55eeb50cabc988f932c6ac9c6a22d7af2b9c
sfzgzs <ghasemi.fateme33@gmail.com>
2025-08-13 23:46:21 -0400

    Add Synatx for While (Bool)

--- 12-Example.lean
+++ 12-Example.lean
@@ -1,18 +1,16 @@
 open Classical
 
-def Env := String → Nat
 
-def Env.insert (v : String) (n : Nat) (env : Env) : Env :=
-  fun x => if x = v then n else env x
+inductive Term where
+  | var : String → Term
+  | true
+  | false
+  | and : Term → Term → Term
+  | or : Term → Term → Term
 
 inductive Stmt where
   | skip : Stmt
-  | assign : String → (Env → Nat) → Stmt
+  | assign : String → Term → Stmt
   | seq : Stmt → Stmt → Stmt
-  | ifThenElse : (Env → Bool) → Stmt → Stmt → Stmt
-
-noncomputable def evalStmt : Stmt → Env → Env
-  | .skip, env => env
-  | .assign v e, env => env.insert v (e env)
-  | .seq S1 S2, env => evalStmt S2 (evalStmt S1 env)
-  | .ifThenElse b S1 S2, env => if b env then evalStmt S1 env else evalStmt S2 env
+  | ifThenElse : Term → Stmt → Stmt → Stmt
+  -- | while : Term → Stmt → Stmt
